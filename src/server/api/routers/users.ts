e8b84d3787a6ef18d3fd74b215a26a8d63a948e8
Fahad Jabr <fahd10001000@windowslive.com>
2025-10-30 01:12:21 +0300

    fixed

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -10,21 +10,28 @@ const workos = new WorkOS(env.WORKOS_API_KEY);
  */
 export const usersRouter = createTRPCRouter({
   getTechnicians: protectedProcedure.query(async ({ ctx }) => {
+    if (!ctx.roles?.includes("technician")) throw new Error("Forbidden");
+
     const organization = ctx.organizationId;
     const users = await workos.userManagement.listOrganizationMemberships({
       organizationId: organization,
     });
     const technicianMembers = users.data.filter(
       (user) =>
-        user.roles?.some(
-          (role) =>
-            "slug" in role && (role as { slug?: string }).slug === "technician",
-        ) ?? false,
+        // skip signedin user ctx.user
+        (user.userId !== ctx.user.id &&
+          user.roles?.some(
+            (role) => (role as { slug?: string }).slug === "technician",
+          )) ??
+        false,
     );
-
     const technicians = await Promise.all(
-      technicianMembers.map((user) => workos.userManagement.getUser(user.id)),
+      technicianMembers.map((user) =>
+        workos.userManagement.getUser(user.userId),
+      ),
     );
+    // append current user to technicians
+    technicians.push(ctx.user);
     return technicians;
   }),
 });
