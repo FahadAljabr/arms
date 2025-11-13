f3a8b89a17f32dbff51afc2034bceb0be550670e
Fahad Jabr <fahd10001000@windowslive.com>
2025-11-14 02:20:45 +0300

    mapviewbar

--- src/components/main-navigation.tsx
+++ src/components/main-navigation.tsx
@@ -72,16 +72,19 @@ export async function MainNavigation() {
                 </NavigationMenuLink>
               </NavigationMenuItem>
 
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    href="/map"
-                    className="group bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-                  >
-                    Asset Map
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
+              {(roles?.includes("admin") ||
+                roles?.includes("major-general")) && (
+                <NavigationMenuItem>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href="/map"
+                      className="group bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                    >
+                      Asset Map
+                    </Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              )}
 
               <NavigationMenuItem>
                 <NavigationMenuLink asChild>
